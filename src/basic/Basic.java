49dc79c71b7a01e4de8ea6064d62135c1deed8ae
gimgihyeon <gihyeon6394@gmail.com>
2023-06-08 13:45:11 +0900

    ing : 기본 프로그래밍 모델 작업 중

---
+++ src/basic/Basic.java
@@ -0,0 +1,24 @@
+package basic;
+
+public class Basic {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+    /**
+     * 이진 탐색의 재귀적 구현
+     */
+
+    public static int rank(int key, int[] a) {
+        return rank(key, a, 0, a.length - 1);
+    }
+
+    public static int rank(int key, int[] a, int lo, int hi) {
+        if (lo > hi) return -1;
+        int mid = lo + (hi - lo) / 2;
+        if (key < a[mid]) return rank(key, a, lo, mid - 1);
+        else if (key > a[mid]) return rank(key, a, mid + 1, hi);
+        else return mid;
+    }
+}
