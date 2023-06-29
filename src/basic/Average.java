14b36e7489c2f72a629b021ae4366d8643b4ce70
gimgihyeon <gihyeon6394@gmail.com>
2023-06-29 15:27:03 +0900

    feat : 컴파일 redirection

---
+++ src/basic/Average.java
@@ -0,0 +1,20 @@
+package basic;
+
+import lib.StdIn;
+import lib.StdOut;
+
+public class Average {
+
+    public static void main(String[] args) {
+        double sum = 0.0;
+        int cnt = 0;
+
+        while (!StdIn.isEmpty()) {
+            sum += StdIn.readDouble();
+            cnt++;
+        }
+
+        double avg = sum / cnt;
+        StdOut.printf("Average is %.5f\n", avg);
+    }
+}
---
+++ src/basic/RandomSeq.java
@@ -0,0 +1,21 @@
+package basic;
+
+import lib.StdOut;
+import lib.StdRandom;
+
+public class RandomSeq {
+
+    public static void main(String[] args) {
+        int N = Integer.parseInt(args[0]);
+        double lo = Double.parseDouble(args[1]);
+        double hi = Double.parseDouble(args[2]);
+
+        // 10, 100, 1000, 10000, 100000, 1000000, ...
+
+        // 랜덤 수 할당
+        for (int i = 0; i < N; i++) {
+            double x = StdRandom.uniform(lo, hi);
+            StdOut.printf("%.2f\n", x);
+        }
+    }
+}
